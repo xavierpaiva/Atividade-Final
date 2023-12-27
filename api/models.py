c67bc99baf006162c471a29466895011f1a6c8a2
Xavierpaiva <Xavier.paiva@escolar.ifrn.edu.br>
2023-12-27 11:59:22 -0300

    Inicial

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    addres = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    company = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Todo(models.Model):
+    userld = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField()
+
+class Post(models.Model):
+    userld = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    postld = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100) 
+    body = models.IntegerField()
---
+++ api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import User, Todo, Post, Comment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
