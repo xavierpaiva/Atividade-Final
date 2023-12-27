e02994df40f30afd3dd155344bffb7bb3d0b0816
Xavierpaiva <Xavier.paiva@escolar.ifrn.edu.br>
2023-12-27 11:59:39 -0300

    inicial

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Todo, Post, Comment
+
+admin.site.register(User)
+admin.site.register(Todo)
+admin.site.register(Post)
+admin.site.register(Comment)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Todo, Post, Comment
+from .serializers import UserSerializer, TodoSerializer, PostSerializer,  CommentSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
